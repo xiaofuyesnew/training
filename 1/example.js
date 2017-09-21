84fae02418f765b45f86627ee5150e59ea7873a2
xiaofuyesnew <xiaofuyesnew@qq.com>
2017-09-21 11:53:47 +0800

    更新文件

---
+++ 1/example.js
@@ -0,0 +1,19 @@
+$('p')   //选择页面上所有的p标签
+$('ul li')    //选择所有ul标签下的li标签
+
+$('.red')   //选择页面上所有类名为red的标签
+$('p.red')    //选择所有拥有red类的p标签
+
+$('#second')    //选择id为second的元素
+$('.red:first')  //第一个类名为red的元素
+
+$('li:first')        //所有符合条件的li标签的第一个标签
+$('li:last')         //所有符合条件的li标签的最后一个标签
+$('li:first-child')  //符合条件的作为第一个子元素的所有li标签
+/**   :even 奇数   :odd  偶数 */
+
+$('input[type="text"]')    //拥有属性为type并且属性值为text的元素
+$('a[href]')    //拥有属性为href的a标签
+
+$('ul>li')    //ul下的标签为li的子元素
+
